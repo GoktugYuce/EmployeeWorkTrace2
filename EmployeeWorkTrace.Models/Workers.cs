b86227a6d4e0a408ad45ef7649873a9914fa3b2c
Göktuğ Yüce <goktugyuce2002@hotmail.com>
2024-03-01 11:28:10 +0300

    PRJ-M-1 UnitOfWork eklemesi yapıldı. Workers eklendi ve Assignee kısmı selectable yapıldı.

---
+++ EmployeeWorkTrace.Models/Workers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWorkTrace.Models
+{
+    public class Workers
+    {
+        [Key]
+        public int WorkerId { get; set; }
+        public string WorkerName { get; set; }
+        public int WorkerPassword { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+    }
+}
--- EmployeeWorkTrace.Models/Works.cs
+++ EmployeeWorkTrace.Models/Works.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeWorkTrace.Models
 {
@@ -11,13 +13,21 @@ namespace EmployeeWorkTrace.Models
         [MaxLength(30)]
         [DisplayName("Work Name")]
         public string WorkName { get; set; }
-        public string Assignee { get; set; }
-        public DateTime WorkCreationDate { get; set; }
-        public DateTime WorkEndDate { get; set; }
-
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Worker Name")]
+        [ValidateNever]
+        public string WorkerName { get; set; }
+        public DateTime? WorkCreationDate { get; set; }
+        public DateTime? WorkEndDate { get; set; }
         public Works()
         {
             WorkCreationDate = DateTime.Now;
         }
+
+        public int WorkerId { get; set; }
+        [ForeignKey("WorkerId")]
+        [ValidateNever]
+        public Workers Workers { get; set; }
     }
 }
